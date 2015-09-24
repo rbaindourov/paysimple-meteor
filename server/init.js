8418c5008237417af516c4e8fb506d54c3dae225
Robert Baindourov <robert@webdesigner.la>
2015-09-24 15:13:02 -0700

    Saving progress on server side polling

--- server/init.js
+++ server/init.js
@@ -1,13 +1,22 @@
 
+  let crypto = Npm.require('crypto')
+
   Meteor.startup( () => {
       console.log( 'startup');
 
       Meteor.setInterval( ()=>{
-        console.log( 'polling paysimple');
-        /*
-        request.get('', (err, response, body)=>{
-          console.log(err, response, body );
+
+        let iso =  new Date().toISOString()
+        let hmac = crypto.createHmac('sha256', Meteor.settings.paysimple.secret).update(new Buffer( iso ).toString() ).digest('base64')
+
+        let headers = {
+          'Authorization': `PSSERVER AccessId = ${Meteor.settings.paysimple.username}; Timestamp = ${iso}; Signature = ${hmac}`
+        }
+
+        request.get({url:Meteor.settings.paysimple.url, strictSSL:false, headers:headers}, (err, response, body)=>{
+          console.log( body );
         });
-        */
-      }, 50000)
+
+      }, 5000)
+
   });
