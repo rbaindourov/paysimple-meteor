bff2e2d60e3aea379d93b7bca0c279a50da1bcdb
Robert Baindourov <robert@webdesigner.la>
2015-10-20 10:48:27 -0700

    saving styling changes

--- components/LineItem.jsx
+++ components/LineItem.jsx
@@ -5,10 +5,10 @@ LineItem = React.createClass({
       <tr>
         <td>{this.props.item.Id}</td>
         <td>{this.props.item.CustomerId}</td>
-        <td>{this.props.item.CustomerFirstName}</td>
-        <td>{this.props.item.CustomerLastName}</td>
-        <td>{this.props.item.CustomerCompany}</td>
-        <td>{this.props.item.Status}</td>
+        <td className="left">{this.props.item.CustomerFirstName}</td>
+        <td className="left">{this.props.item.CustomerLastName}</td>
+        <td className="left">{this.props.item.CustomerCompany}</td>
+        <td className="left">{this.props.item.Status}</td>
         <td>{this.props.item.Amount}</td>
         <td>{this.props.item.CreatedOn}</td>
         <td>{this.props.item.LastModified}</td>
